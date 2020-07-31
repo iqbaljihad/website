ea746fbde7dd02adab1b9147c9c953b9e48f0d76
Jihad Iqbal <jihadi@amazon.com>
2020-07-30 23:16:16 -0400

    book formatting

--- src/components/table-of-contents.js
+++ src/components/table-of-contents.js
@@ -1,5 +1,24 @@
-import React from "react"
+// import React from "react"
+// import { graphql } from "gatsby"
 
-export default function TableOfContents() {
-  ;<h1>Test</h1>
-}
+// export default function TableOfContents() {
+//   ; <h1>Test</h1>
+// }
+
+// export const query = graphql`
+//   query PostBySlug($url: String!) {
+//     mdx(fields: { url: { eq: $url } }) {
+//       frontmatter {
+//         title
+//         date(formatString: "YYYY MMMM Do")
+//       }
+//       body
+//       excerpt
+//       tableOfContents
+//       timeToRead
+//       fields {
+//         slug
+//       }
+//     }
+//   }
+// `
