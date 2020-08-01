7d42685f691124e1591e9dc182dc563e61b2a641
Jihad Iqbal <jihadi@amazon.com>
2020-08-01 15:55:00 -0400

    modified library

--- src/templates/markdown-post.js
+++ src/templates/markdown-post.js
@@ -2,13 +2,20 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { css } from "@emotion/core"
 
 export default function MarkdownPost({ data }) {
   const post = data.mdx
 
   return (
     <Layout>
-      <div> 
+      <div>
+        <div>
+          <h1>{post.frontmatter.title}</h1>
+          <h2 css={css`
+              color: #bbb;
+            `}>{post.frontmatter.author}</h2>
+        </div>
         <MDXRenderer>{post.body}</MDXRenderer>
       </div>
     </Layout>
